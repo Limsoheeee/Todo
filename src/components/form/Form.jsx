4b1fd5725e4d513939b989becc54d172745158c4
Limsohee <v_vsoi@naver.com>
2022-10-02 16:08:14 +0900

    form에 title, content	받아와 formValue에 배열로 합치기

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,23 +2,35 @@ import React, { useState } from "react";
 import "./style.css";
 
 const Form = () => {
-
-  const InitValue = {id:0, title:"title", content:"",};
-  const [value,setValue] = useState(InitValue);
-   const onChangeHandler = (e) =>{
-    const inputValue = e.target.value;
-    console.log(e)
-    setValue(inputValue)
+  
+  const [formValue,setValue] = useState([]);     //폼밸류 배열처리 온클릭시 처리
+  const [title,setTitle] = useState("");
+  const [content,setContent] = useState("");
+ 
+   const onChangeHandlerTitle = (e) =>{   //타이틀 밸류 받아오기
+    const inputTitle = e.target.value;
+    setTitle(inputTitle)
    };
+
+   const onChangeHandlerContent = (e) =>{  //콘텐트 밸류 받아오기
+    const inputContent = e.target.value; 
+    setContent(inputContent)
+   };
+
+    const onClickAddList = () => {
+      if(title&&content){
+        setValue=([...formValue,{id:formValue.length+1,title,content,isDone:false}])
+      }
+   }
 
   return (
     <form className="add-form">
       <div className="input-group">
         <label className="form-label">제목</label>
-        <input type="text" className="add-input input-group" name="title" onChange={onChangeHandler} value = {value} />
+        <input type="text" className="add-input input-group" name="title" onChange={onChangeHandlerTitle} value = {value} />
         <label className="form-label">내용</label>
-        <input type="text" className="add-input input-group" />
-        <button className="add-button">추가</button>
+        <input type="text" className="add-input input-group" name="content" onChange={onChangeHandlerContent} value = {value}/>
+        <button className="add-button" onClick={onClickAddList}>추가</button>
       </div>
     </form>
   );
