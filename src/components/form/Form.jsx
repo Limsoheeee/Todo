239e1734b34937026d18905342e878f8ffc4d3be
Limsohee <v_vsoi@naver.com>
2022-10-04 12:13:47 +0900

    폼의 제목, 콘텐트의 인풋값을 하나로 받아오는 것으로 합침

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -10,35 +10,39 @@ const Form = ({ list, setList }) => {
   };
   // const [formValue, setValue] = useState([mergy]); //폼밸류 배열처리 온클릭시 처리
   // //폼밸류와 셋밸류가 투두리스트의 리스트가되지만, 여기서 작성안하고 투두리스트에서 작성
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-
-  const onChangeHandlerTitle = (e) => {
+  const [toDo, setTodo] = useState([mergy]);
+  // const [content, setContent] = useState("");
+  const { title, content } = toDo; //비구조화할당
+  console.log(toDo)
+  const onChangeHandler = (e) => {
     //타이틀 밸류 받아오기
-    const inputTitle = e.target.value;
-    setTitle(inputTitle);
+    const {title,content} = e.target;
+     setTodo({
+      ...toDo,
+      [title]:title,
+      [content]:content,
+     });
   };
-
-  const onChangeHandlerContent = (e) => {
-    //콘텐트 밸류 받아오기
-    const inputContent = e.target.value;
-    setContent(inputContent);
-  };
+  // const onChangeHandlerContent = (e) => {
+  //   //콘텐트 밸류 받아오기
+  //   const inputContent = e.target.value;
+  //   setContent(inputContent);
+  // };
   const onSubmitHandler = (event) => {
     event.preventDefault();
     //버튼추가하면 폼밸류생성
-    if (title && content) { //제목과 내용을 둘다 받아야지만 배열에 정보가 들어감
+    if (setTodo!=="") { //제목과 내용을 둘다 받아야지만 배열에 정보가 들어감
       setList([
         ...list,
-        {...mergy,id:list.length+1 ,title,content},
+        {...toDo,id:list.length+1 ,title,content},
         // { id: formValue.length + 1, title, content, isDone: false },
       ]);
-      setTitle("");   //제목과 내용을 추가 누른 후 값을 초기화
-      setContent("");
+      setTodo("");   //제목과 내용을 추가 누른 후 값을 초기화
 
     }
     else{
       alert('제목과 내용을 모두 입력해주세요.')
+      setTodo("")
     }
   };
 
@@ -52,7 +56,8 @@ const Form = ({ list, setList }) => {
           type="text"
           className="add-input input-group"
           name="title"
-          onChange={onChangeHandlerTitle}
+          placeholder="제목"
+          onChange={onChangeHandler}
           value={title}
         />
         <label className="form-label">내용</label>
@@ -60,7 +65,8 @@ const Form = ({ list, setList }) => {
           type="text"
           className="add-input input-group"
           name="content"
-          onChange={onChangeHandlerContent}
+          placeholder="내용"
+          onChange={onChangeHandler}
           value={content}
         />
         <button type="submit" className="add-button" >
