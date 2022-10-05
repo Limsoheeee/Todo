90bf449e14b0c08a87762a024f14e580c9cc5209
Limsohee <v_vsoi@naver.com>
2022-10-05 13:14:49 +0900

    삭제 취소버튼 생성

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import "./style.css";
 
-const Todo = ({ title, content,List, setList }) => {
+const Todo = ({ id, title, content, isDone, list, setList }) => {
+  const checkHandler = () => {
+    const result = list.map((item) => {
+      if (item.id === id) {
+        return { ...item, isDone: !item.isDone };
+      } else {
+        return item;
+      }
+    });
+
+    setList(result);
+  };
+
+  const deleteHandler = () => {
+    const result = list.filter((item) => {
+      if (item.id === id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    setList(result);
+  };
 
   return (
     <li className="todo-item">
       <h2>{title}</h2>
       <p>{content}</p>
       <div className="button-set">
-      <button className="remove-button">삭제하기</button>
-      <button className="check-button">완료</button>
+        <button className="remove-button" onClick={deleteHandler}>
+          삭제하기
+        </button>
+        <button className="check-button" onClick={checkHandler}>
+          {isDone ? "취소" : "완료"}
+        </button>
       </div>
-         </li>
+    </li>
   );
 };
 
